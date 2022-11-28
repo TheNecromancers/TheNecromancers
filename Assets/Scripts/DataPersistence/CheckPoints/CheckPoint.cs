b517b5a14739d927077ffcf5675abd8247871604
Alessio Vulpinari <alessiovulpinari99@gmail.com>
2022-11-28 20:31:57 +0100

    CheckPoint

---
+++ Assets/Scripts/DataPersistence/CheckPoints/CheckPoint.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    private GameMaster gm;
+
+    private void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            gm.lastCheckPointPos = transform.position;
+            Debug.Log("Pestato");
+        }
+    }
+}
