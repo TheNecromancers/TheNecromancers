c6ff8d67ddffc6137b4b45ef018f81d80a96b5c4
Francesco Brischetto <francesco_brischetto@hotmail.it>
2022-11-29 12:58:22 +0100

    Refined the exploration ability and added cooldown
    Refined the exploration ability and added cooldown

--- Assets/Scripts/Gameplay/Player/AbilitySystemManager.cs
+++ Assets/Scripts/Gameplay/Player/AbilitySystemManager.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class AbilitySystemManager : MonoBehaviour
 {
+    [field: Header("Exploration Ability")]
+
+    [SerializeField] float MinHeight;
+    [SerializeField] float MaxHeight;
+
     [SerializeField] float MaxRange;
     [SerializeField] float MinRange;
 
@@ -11,10 +16,13 @@ public class AbilitySystemManager : MonoBehaviour
     [SerializeField] float MinIntensity;
 
     [SerializeField] float ModifierSpeed;
-    [SerializeField] float Timer;
+    [SerializeField] float TimerAfterReachingMax;
+    [SerializeField] float ExplorationCooldown;
 
     private bool explorationRunning;
-
+    private float intensityWeight;
+    private float heightWeight;
+    private float rangeWeight;
     Light Light;
 
     private void Awake()
@@ -24,8 +32,9 @@ public class AbilitySystemManager : MonoBehaviour
 
     private void Start()
     {
-        Light.range = Mathf.Clamp(Light.range, MinRange, MaxRange);
-        Light.intensity = Mathf.Clamp(Light.intensity, MinIntensity, MaxIntensity);
+        Light.range = MinRange;
+        Light.intensity = MinIntensity;
+        Light.transform.localPosition = new Vector3(Light.transform.localPosition.x, MinHeight, Light.transform.localPosition.z);
         explorationRunning = false;
     }
     public void OnCombactAbility()
@@ -33,31 +42,77 @@ public class AbilitySystemManager : MonoBehaviour
 
     }
 
+    private void CalculateWeights()
+    {
+        float diffHeight = MaxHeight - MinHeight;
+        float diffRange = MaxRange - MinRange;
+        float diffIntensity = MaxIntensity - MinIntensity;
+        float minValue = Mathf.Min(diffHeight, diffRange, diffIntensity);
+        if(minValue == diffHeight)
+        {
+            heightWeight = 1;
+            intensityWeight = diffIntensity / diffHeight;
+            rangeWeight = diffRange / diffHeight;
+        }
+        else if (minValue == diffRange)
+        {
+            rangeWeight = 1;
+            intensityWeight = diffIntensity / diffRange;
+            heightWeight = diffHeight / diffRange;
+        }
+        else
+        {
+            intensityWeight = 1;
+            rangeWeight = diffRange / diffIntensity;
+            heightWeight = diffHeight / diffIntensity;
+        }
+
+    }
+
     public void OnExplorationAbility()
     {
         if (!explorationRunning)
         {
+            CalculateWeights();
             explorationRunning = true;
             StartCoroutine(ExpandLight());
         }  
     }
 
+    private IEnumerator CooldownExplorationAbility()
+    {
+        Debug.Log("Started Cooldown of " + ExplorationCooldown + " Seconds");
+        yield return new WaitForSeconds(ExplorationCooldown);
+        explorationRunning = false;
+        Debug.Log("Ended Cooldown. Exploration Ability Available.");
+    }
+
     public IEnumerator ExpandLight()
     {
+        Vector3 targetLightPosition = new Vector3(Light.transform.localPosition.x, MaxHeight, Light.transform.localPosition.z);
         while(Light.range <= MaxRange || Light.intensity <= MaxIntensity){
-            Light.range += ModifierSpeed * Time.deltaTime;
-            Light.intensity += ModifierSpeed * Time.deltaTime;
+            Light.range += ModifierSpeed * rangeWeight * Time.deltaTime;
+            Light.intensity += ModifierSpeed * intensityWeight * Time.deltaTime;
+            if (Light.transform.localPosition.y < targetLightPosition.y)
+                Light.transform.localPosition = Vector3.MoveTowards(Light.transform.localPosition, targetLightPosition, ModifierSpeed * heightWeight * Time.deltaTime);
+            else
+                Light.transform.localPosition = targetLightPosition;
             yield return null;
         }
-        yield return new WaitForSeconds(Timer);
-        while (Light.range >= MinRange || Light.intensity >= MinIntensity){
-            Light.range -= ModifierSpeed * Time.deltaTime;
-            Light.intensity -= ModifierSpeed * Time.deltaTime;
+        yield return new WaitForSeconds(TimerAfterReachingMax);
+        targetLightPosition = new Vector3(Light.transform.localPosition.x, MinHeight, Light.transform.localPosition.z);
+        while (Light.range >= MinRange && Light.intensity >= MinIntensity){
+            Light.range -= ModifierSpeed * rangeWeight * Time.deltaTime;
+            Light.intensity -= ModifierSpeed * intensityWeight * Time.deltaTime;
+            if (Light.transform.localPosition.y > targetLightPosition.y)
+                Light.transform.localPosition = Vector3.MoveTowards(Light.transform.localPosition, targetLightPosition, ModifierSpeed * heightWeight * Time.deltaTime);
+            else
+                Light.transform.localPosition = targetLightPosition;
             yield return null;
         }
         Light.range = Mathf.Clamp(Light.range, MinRange, MaxRange);
         Light.intensity = Mathf.Clamp(Light.intensity, MinIntensity, MaxIntensity);
-        explorationRunning = false;
+        yield return StartCoroutine(CooldownExplorationAbility()); 
 
     }
 }
