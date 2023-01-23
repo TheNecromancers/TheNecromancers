f17ad9f91743cd24ca1920f53e4e550c80715950
Marco Trapani <108862666+MarcoTrapani@users.noreply.github.com>
2023-01-23 21:30:12 +0100

    Added More Audio Tracks, added Arrays

--- Assets/Scripts/Audio/AudioClips.cs
+++ Assets/Scripts/Audio/AudioClips.cs
@@ -10,4 +10,11 @@ public class AudioClips : ScriptableObject
    [SerializeField] public AudioClip[] Parry;
    [SerializeField] public AudioClip[] Roll;
    [SerializeField] public AudioClip[] CrossbowShoot;
+   [SerializeField] public AudioClip[] Death;
+   [SerializeField] public AudioClip[] BodyDrop;
+   [SerializeField] public AudioClip[] Hurt;
+   [SerializeField] public AudioClip[] EnemyGroan;
+   [SerializeField] public AudioClip[] PowerUp1;
+   [SerializeField] public AudioClip[] PowerUp2;
+
 }
