150d4732dfd84bf20184837599bb44b58950a840
Pietro <pietro.digiovanni.dev@gmail.com>
2023-02-03 18:06:24 +0100

    Enemy disabled after 5 seconds on death

--- Assets/Scripts/StateMachines/Enemy/EnemyDeadState.cs
+++ Assets/Scripts/StateMachines/Enemy/EnemyDeadState.cs
@@ -6,22 +6,30 @@ namespace TheNecromancers.StateMachine.Enemy
     {
         private readonly int DieHash = Animator.StringToHash("Die");
         private const float CrossFadeduration = 0.1f;
+
+        float disableEnemyDelay = 5f;
         public EnemyDeadState(EnemyStateMachine stateMachine) : base(stateMachine) { }
 
         public override void Enter()
         {
             Debug.Log("Nemico Morto");
+            disableEnemyDelay += Time.time;
             stateMachine.Animator.CrossFadeInFixedTime(DieHash, CrossFadeduration);
             stateMachine.WeaponLogic.gameObject.SetActive(false);
             stateMachine.Controller.enabled = false;
             GameObject.Destroy(stateMachine.Target);
-            //GameObject.Destroy(stateMachine.gameObject, 5f);
-            stateMachine.gameObject.SetActive(false);
 
             AudioManager.Instance.PlayRandomClip(stateMachine.AudioClips.Death);
         }
 
-        public override void Tick(float deltaTime) { }
+        public override void Tick(float deltaTime) 
+        { 
+            if(Time.time > disableEnemyDelay)
+            {
+                stateMachine.gameObject.SetActive(false);
+                //GameObject.Destroy(stateMachine.gameObject, 5f);
+            }
+        }
 
         public override void Exit() { }
     }
