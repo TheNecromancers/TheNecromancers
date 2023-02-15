1015911c198a475e65a242076d6c036380850836
MasterPiccio <103529102+MasterPiccio@users.noreply.github.com>
2023-02-16 00:13:44 +0100

    Pause Menu Navigation
    Fixed Navigation with controller on PauseMenu.
    Explicit Navigation setting and prevent deselection method

--- Assets/Scripts/Managers/MainMenuManager.cs
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -108,25 +108,6 @@ public class MainMenuManager : MonoBehaviour
         SceneManager.LoadScene("TutorialCommands");
         
     }
-/*     public class PreventDeselectionGroup : MonoBehaviour
-{
-    EventSystem evt;
-
-    private void Start()
-    {
-        evt = EventSystem.current;
-    }
-
-    GameObject sel;
-
-    private void Update()
-    {
-        if (evt.currentSelectedGameObject != null && evt.currentSelectedGameObject != sel)
-            sel = evt.currentSelectedGameObject;
-        else if (sel != null && evt.currentSelectedGameObject == null)
-            evt.SetSelectedGameObject(sel);
-    }
-} */
 
     public void PreventDeselection()
     {
